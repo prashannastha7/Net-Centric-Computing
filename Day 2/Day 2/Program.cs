1b5cdb7cede5f828b46cf90132f1f12dfb9ea6f4
prashannastha7 <prashannabdrshrestha711@gmail.com>
2024-04-10 23:43:10 +0545

    Inheritance

---
+++ Day 2/Day 2/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("---  Example on single level inheritance  ----");
+            //creating obj of sub class
+            Teacher t1 = new Teacher();
+            //using parent properties
+            t1.setEmployee(1, "hari", "ktm", "IT");
+            t1.displayEmployee();
+            //using own properties
+            t1.setTeacher("Networking", "50000");
+            t1.displayTeacher();
+            //creating obj of parent class (super class)
+            Employee e1 = new Employee();
+            e1.setEmployee(2, "sam", "pkr", "Management");
+            e1.displayEmployee();
+            //Employee cann't access to Teacher properties
+
+
+            Console.WriteLine("\n\n----------------------------\n\n");
+
+            Console.WriteLine("----  MultilevelInhertance  ----");
+                //creating obj of grandchild (ElectricBike)
+                ElectricBike eb = new ElectricBike();
+            eb.Reg = 101; //value diyo set called huncha
+            eb.Name = "niu";
+            eb.Model = "new";
+            //using parent property (Bike)
+            eb.Speed = "40";
+            //using own property
+            eb.Range = "50";
+            Console.WriteLine("reg is " + eb.Reg); //get called
+            Console.WriteLine("name is " + eb.Name); //get called
+            Console.WriteLine("model is " + eb.Model); //get called
+            Console.WriteLine("speed is " + eb.Speed); //get called
+            Console.WriteLine("range is " + eb.Range); //get called
+
+            //creating object of Bike
+            Bike bi = new Bike();
+            //using TwoWheeler property
+            bi.Reg = 102;
+            bi.Name = "pulsar";
+            bi.Model = "old";
+            bi.Speed = "200"; //own property
+            //bi.Range = "40"; cannot access
+            Console.WriteLine("reg is bike " + bi.Reg);
+            Console.WriteLine("name of bike " + bi.Name);
+            Console.WriteLine("model of bike " + bi.Model);
+            Console.WriteLine("speed of bike " + bi.Speed);
+
+            Console.WriteLine("");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Day 2/Day 2/SingleLevelInheritance.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Day_2
+{
+    class Employee
+    {
+        private int id;
+        private string name;
+        private string address;
+        private string department;
+        //method to initialize instance variable
+
+        public void setEmployee(int id, string name, string address, string department)
+        {
+            this.id = id;
+            this.name = name;
+            this.address = address;
+            this.department = department;
+        }
+        public void displayEmployee()
+        {
+            Console.WriteLine(" id is " + this.id);
+            Console.WriteLine(" name is " + this.name);
+            Console.WriteLine(" address is " + this.address);
+            Console.WriteLine(" department is " + this.department);
+        }
+    }
+    //inheritance Employee by teacher
+    class Teacher : Employee //teacher is sub class , Employee is super class
+    {
+        private string specialization;
+        private string salary;
+
+        public void setTeacher(string specialization, string sal)
+        {
+            this.specialization = specialization;
+            salary = sal; // variable name different then no use of this.        
+        }
+        public void displayTeacher()
+        {
+            Console.WriteLine("Specialization is " + this.specialization);
+            Console.WriteLine("salary is " + salary);
+        }
+    }
+}
