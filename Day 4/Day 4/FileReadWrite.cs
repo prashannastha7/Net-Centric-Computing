070d7b5b5a40e0c3f8c638a8be3abe302e117f35
prashannastha7 <prashannabdrshrestha711@gmail.com>
2024-04-16 23:47:36 +0545

    File R&W

---
+++ Day 4/Day 4/FileReadWrite.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography; // for file handling
+namespace Day_4
+{
+    class FileReadWrite
+    {
+        public void writeFile()
+        {
+            //to create a file for writing, StreamWriter is used 
+            StreamWriter sw = new StreamWriter("A://abc.txt");
+            Console.WriteLine("Enter text to insert");
+            string data = Console.ReadLine();
+            sw.WriteLine(data);
+            sw.Flush();//clear buffer and write to stream
+            sw.Close();
+        }
+
+        public void readFile()
+        {
+            StreamReader sr = new StreamReader("A://abc.txt");
+            string data1 = sr.ReadLine();
+
+
+            Console.WriteLine(data1);
+
+            sr.Close();
+        }
+    }
+}
